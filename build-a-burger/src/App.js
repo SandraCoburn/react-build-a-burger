2bfcfe9e4cbd1dabc2d32cace8ae0fcd5bc05042
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-12 12:25:33 -0700

    Changed file structure to add layout to higher order components

--- build-a-burger/src/App.js
+++ build-a-burger/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./components/Layout/Layout";
+import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout_story from "./components/StorySquad/Pages/Layout_story";
 
