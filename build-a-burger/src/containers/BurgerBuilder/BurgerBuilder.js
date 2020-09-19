1b09068b7d7a06c08ca3e0e061d436ef70d303b1
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-19 11:36:07 -0700

    Added navigation back and to Next Page by using router history.push

--- build-a-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ build-a-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,29 +81,30 @@ class BurgerBuilder extends Component {
   };
   purchaseContinueHandler = () => {
     // alert("You continued!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Max coburn",
-        address: {
-          street: "test street 1",
-          zipCode: "12345",
-          country: "US",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethod: "fastest",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Max coburn",
+    //     address: {
+    //       street: "test street 1",
+    //       zipCode: "12345",
+    //       country: "US",
+    //     },
+    //     email: "test@test.com",
+    //   },
+    //   deliveryMethod: "fastest",
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false });
+    //   });
+    this.props.history.push("/checkout");
   };
 
   render() {
