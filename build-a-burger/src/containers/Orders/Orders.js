f098e99b1fb4df8b009d3c449b5ceb4f60c908a2
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-19 17:21:07 -0700

    Added axios get request in Orders.js to fetch orders from database

--- build-a-burger/src/containers/Orders/Orders.js
+++ build-a-burger/src/containers/Orders/Orders.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import axios from "../../axios.orders";
 
 import Order from "../../components/Order/Order";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler"; //wrap component to handle errors
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        //Firebase returns an object as a response, to turn it into array:
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key }); //copy of data to get id
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((er) => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
@@ -12,4 +33,4 @@ class Orders extends Component {
     );
   }
 }
-export default Orders;
+export default withErrorHandler(Orders);
