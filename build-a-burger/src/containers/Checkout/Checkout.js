284714332e1f032190b2fb3408d38fe8a7e52283
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-25 06:35:57 -0700

    Combined reducers

--- build-a-burger/src/containers/Checkout/Checkout.js
+++ build-a-burger/src/containers/Checkout/Checkout.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
 
 class Checkout extends Component {
-  //With Redux, we don't need component will mount to acces params
-  // componentWillMount() {
-  //   const query = new URLSearchParams(this.props.location.search);
-  //   const ingredients = {};
-  //   let price = 0;
-  //   for (let param of query.entries()) {
-  //     //["salad", "1"]
-  //     if (param[0] === "price") {
-  //       price = param[1];
-  //     } else {
-  //       ingredients[param[0]] = +param[1]; //adding a + turns it into a number
-  //     }
-  //   }
-  //   this.setState({ ingredients: ingredients, totalPrice: price });
-  // }
-
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
@@ -31,32 +15,29 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinue={this.checkoutContinuedHandler}
-        />
-        <Route
-          path={this.props.match.path + "/contact-data"}
-          component={ContactData} //with Redux we don't need to render the component
-          // render={(props) => (
-          //   <ContactData
-          //     ingredients={this.props.ings}
-          //     price={this.state.totalPrice}
-          //     {...props}
-          //   />
-          // )}
-        />
-      </div>
-    );
+    const summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinue={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={this.props.match.path + "/contact-data"}
+            component={ContacData}
+          />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
+    ings: state.burgerBuilder.ingredients,
   };
 };
 
--- build-a-burger/src/containers/Checkout/ContactData/ContactData.js
+++ build-a-burger/src/containers/Checkout/ContactData/ContactData.js
@@ -89,6 +89,7 @@ class ContactData extends Component {
         },
         value: "fastest",
         validation: {},
+        valid: true,
       },
     },
     formIsValid: false,
@@ -193,9 +194,9 @@ class ContactData extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading,
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.orders.loading,
   };
 };
 
