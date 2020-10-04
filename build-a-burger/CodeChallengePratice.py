a21edb7eb4d972a5d800d6fbb2c3a37170fe96eb
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-04 08:08:32 -0700

    Testing the reducer

--- build-a-burger/CodeChallengePratice.py
+++ build-a-burger/CodeChallengePratice.py
@@ -4,7 +4,7 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-print(fibonacci(12))
+#print(fibonacci(12))
 
 def iterative_fibonacci(index):
     first_num = 0
@@ -28,7 +28,7 @@ def dynamic_fibonacci(n):
         if n < 2:
             return n
         else:
-            cache[n] = dynamic_fibonacci(n-1) + dynamic_fibonacci[n-2]
+            cache[n] = dynamic_fibonacci(n-1) + dynamic_fibonacci(n-2)
             return cache[n]
 
 def factorial(num):
@@ -42,3 +42,44 @@ def recursive_factorial(num):
         return 1
     else:
         return num * recursive_factorial(num -1)
+
+def gerund(string):
+    #  st = [s for s in string]
+    #  print(st)
+    #  ing = ["i", "n", "g"]
+
+     if string[-3:] == "ing":
+         return "to " + string[:-3]
+
+print(gerund("building"))
+
+def commafy(lst):
+    lst[-1] = "and " + lst[-1] 
+    return ", ".join(lst)
+
+print(commafy(["hello", "apple", "list"]))
+
+def how_many(lst):
+    if lst[0] == 1:
+        return f'there is {str(lst[0])} {lst[-1]}'
+    else:
+        return f'there are {str(lst[0])} {lst[-1]}s.'
+
+print(how_many([2, "dolls"]))
+
+def abrieviator(string):
+    if len(string) < 5:
+        return string
+    else:
+        return string[:4]
+print(abrieviator("enigmatic"))
+
+def lookup_states_abr(abreviation):
+    ## make a dictionary to hold the states and abbreviations
+    states = {
+        "CA": "California",
+        "NY": "New York"
+    }
+
+    return states[abreviation]
+print(lookup_states_abr("CA"))
